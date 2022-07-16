62a0f65308df9bcc2be3f25e49f51893bc4becd5
Chloe <gtalk.with.nani@gmail.com>
2022-07-17 03:19:15 +0900

    시리즈를 분기하다

---
+++ src/apis/series.js
@@ -0,0 +1,12 @@
+import { v4 as uuidv4 } from 'uuid';
+
+const series = [
+  {
+    id: uuidv4(),
+    thumbnail: "/series/devil-styled-sheets.png",
+    category: "Devil Styled Sheets",
+    url: "/devil-styled-sheets",
+  },
+]
+
+export default series;
--- src/pages/devil-styled-sheets.js
+++ src/pages/devil-styled-sheets.js
@@ -1,5 +1,5 @@
 import * as React from "react"
-import series from '../apis/series'
+import posts from '../apis/posts'
 import SEO from '../components/utilities/withHead'
 import Container from '../components/layout'
 import * as styles from '../styles/Home.module.sass'
@@ -7,33 +7,36 @@ import Profile from '../components/pages/profile'
 import LinkButton from '../components/utilities/linkButton'
 import Route from '../components/pages/route'
 
-function IndexPage() {
+function DevilStyledSheetsPage() {
   return (
     <Container>
       <SEO
-        title="Series"
+        title="Home"
       />
       <main className={styles['mainPage']}>
         <Profile />
         <Route route='series' />
+        <h2>Devil Styled Sheets</h2>
         <ul>
-          {series.map(sery => (
-            <li key={sery.id}>
-              <LinkButton href={sery.url}>
+          {posts.filter(post => post.category === 'Devil Styled Sheets').map(post =>
+            <li key={post.id}>
+              <LinkButton href={post.url}>
                 <div className={styles['listThumbnail']}>
-                  <i style={{ backgroundImage: `url(${sery.thumbnail}` }} />
-                  {sery.badge && (
+                  <i style={{ backgroundImage: `url(${post.thumbnail}` }} />
+                  {post.badge && (
                     <span aria-label="New Badge">새글</span>
                   )}
                 </div>
-                <strong>{sery.category}</strong>
+                <strong>{post.subject}</strong>
+                <p>{post.description}</p>
+                <time>{post.datetime}</time>
               </LinkButton>
             </li>
-          ))}
+          )}
         </ul>
       </main>
     </Container>
   )
 }
 
-export default IndexPage
+export default DevilStyledSheetsPage
